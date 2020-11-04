cc1a42e90c36f4c6360b1ff4ee510ad51e86728a
gamblingrepairman <awaite67@gmail.com>
2020-11-04 04:25:54 -0500

    worked one part 1 and part 2

--- todoapp.py
+++ todoapp.py
@@ -1,34 +1,37 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
 
 app = Flask(__name__)
+fakelist = [["x", "x", "x"], ["y", "y", "y"], ["z", "z", "z"]]
 
 
 @app.route('/')
 def hello_world():
     author = "beast mode beast mode"
     name = "apex predator"
-    return render_template('index.html', author=author, name=name)
+    return render_template('index.html', author=author, name=name, fakelist=fakelist)
 
 
-@app.route('/submit')
-def hello_world():
-    author = "/submit"
-    name = "/submit"
-    return render_template('index.html', author=author, name=name)
+@app.route('/submit', methods = ['POST'])
+def submit():
+    task = request.form['task']
+    email = request.form['email']
+    priority = request.form['priority']
+    fakelist.append([task, email, priority])
+    return redirect('/')
 
 
+"""
 @app.route('/clear')
 def hello_world():
     author = "/submit"
     name = "/submit"
     return render_template('index.html', author=author, name=name)
-
+"""
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
 
-
-#export FLASK_APP=hello.py
+# export FLASK_APP=hello.py
 # flask run
 # https://realpython.com/the-model-view-controller-mvc-paradigm-summarized-with-legos/
 # http: // opentechschool.github.io / python - flask / core / files - templates.html
